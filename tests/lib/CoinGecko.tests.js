1a49a015d9da8f5e4c6bfd59d28e2bff75dcee1a
Levent Saatci <leventsaatci@hotmail.com>
2021-09-10 20:48:20 +0200

    Simple Price endpoint's test added

--- tests/lib/CoinGecko.tests.js
+++ tests/lib/CoinGecko.tests.js
@@ -6,30 +6,26 @@ var should = chai.should();
 
 //Helpers
 const CoinGecko = require('../../lib/CoinGecko');
-
+const CoinGeckoClient = new CoinGecko()
 const shared = require('../shared');
 
+
 describe('CoinGecko', function () {
-  before(function (done) {
-    this.CoinGeckoClient = new CoinGecko();
-
-    done();
-  });
-
+  
   describe('ping', function () {
     before(function (done) {
-      this.CoinGeckoClient.ping().then((data) => {
+      CoinGeckoClient.ping().then((data) => {
         this.data = data;
         done();
       });
     });
 
-    shared.shouldBeAValidRequest();
+    shared.shouldBeAValidRequest(this.data);
   });
 
   describe('Simple Token Price',function (){
     before(function (done){
-      this.CoinGeckoClient.simple.tokenPrice({
+      CoinGeckoClient.simple.tokenPrice({
         id : "ethereum",
         contract_addresses : ['0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48,0xb6ed7644c69416d67b522e20bc294a9a9b405b31'],
         vs_currencies : 'usd'
@@ -42,6 +38,19 @@ describe('CoinGecko', function () {
     shared.shouldBeAValidRequest();
   });
 
+  describe('Simple Price',function (){
+    before(function (done){
+      CoinGeckoClient.simple.price(
+        {ids : "bitcoin"},
+        {vs_currencies : "usd"})
+            .then((data) => {
+                          this.data = data;
+                          done()
+      });
+    });
+
+    shared.shouldBeAValidRequest();
+  });
   
 
   
